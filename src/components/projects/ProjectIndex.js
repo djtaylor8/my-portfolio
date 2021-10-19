cdc423688ca8bac5f34112cbf70705b76920de59
DJ Taylor <dtaylor88@gmail.com>
2021-10-19 14:53:04 -0700

    remove image border

--- src/components/projects/ProjectIndex.js
+++ src/components/projects/ProjectIndex.js
@@ -10,7 +10,7 @@ const useStyles = makeStyles(() => ({
         '&:hover': {
             cursor: 'pointer',
             'box-shadow': 'rgba(0, 0, 0, 0.22) 0px 26px 40px',
-            transform: 'translate3d(0px, -4px, 0px)',
+            transform: 'translate3d(0px, -3px, 0px)',
         }
     },
 }));
@@ -26,8 +26,7 @@ const ProjectIndex = () => {
             <Grid container spacing={2} direction='row' style={{ display: 'flex', maxWidth: 1100, margin: '0 auto', alignItems: 'center', justifyContent: 'center' }}>
                 {projects.map((project) => (
                 <Grid item key={project.id} sm={6}>
-                 <Typography variant='subtitle2' align='center'>{project.title}</Typography>
-                  <Avatar variant='square' sx={{height: '100%', width: '100%', border: '0.1px solid lightgray' }} src={require(`../../static/images/${project.title}.png`).default} className={classes.image} />
+                 <Avatar variant='square' sx={{height: '100%', width: '100%' }} src={require(`../../static/images/${project.title}.png`).default} className={classes.image} />
                 </Grid>
                 ))}
             </Grid>
