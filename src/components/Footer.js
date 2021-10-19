b96189727d02b59a02ac1d12f5444c9523f4996c
DJ Taylor <dtaylor88@gmail.com>
2021-10-19 08:14:54 -0700

    add label to bottom nav buttons, connect to external sites

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,27 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import EmailIcon from '@material-ui/icons/Email';
+import Tooltip from '@mui/material/Tooltip';
+
 
 
 export default function Footer() {
+
+    const handleLinkedIn = () => {
+        window.open('https://www.linkedin.com/in/dj-taylor-3b91ba3a/')
+    }
+
+    const handleGitHub = () => {
+        window.open('https://github.com/djtaylor8')
+    }
+
+    const handleBlog = () => {
+        window.open('https://dj-taylor.medium.com/')
+    }
+
+    const handleEmail =() => {
+        window.open('mailto:dtaylor88@gmail.com')
+    }
 
   return (
     <Box sx={{ width: 500 }}>
@@ -17,12 +35,22 @@ export default function Footer() {
 
       <BottomNavigation
       >
-        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
-        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} />
-        <BottomNavigationAction label="Blog" icon={<LibraryBooksIcon />} />
-        <BottomNavigationAction label="Message" icon={<EmailIcon />} />
+        <Tooltip title='LinkedIn' arrow>
+        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} onClick={handleLinkedIn} />
+        </Tooltip>
+        <Tooltip title='GitHub' arrow>
+        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} onClick={handleGitHub} />
+        </Tooltip>
+        <Tooltip title='Blog' arrow>
+        <BottomNavigationAction label="Blog" icon={<LibraryBooksIcon onClick={handleBlog} />} />
+        </Tooltip>
+        <Tooltip title='Email' arrow>
+        <BottomNavigationAction label="Email" icon={<EmailIcon />} onClick={handleEmail} />
+        </Tooltip>
       </BottomNavigation>
       </Paper>
     </Box>
   );
 }
+
+
