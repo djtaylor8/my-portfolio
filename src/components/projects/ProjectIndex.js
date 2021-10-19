4a8f0af083648b3d16399803dd9791065b599e78
DJ Taylor <dtaylor88@gmail.com>
2021-10-19 14:42:10 -0700

    add hover effect for project index

--- src/components/projects/ProjectIndex.js
+++ src/components/projects/ProjectIndex.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Grid } from '@material-ui/core'
-import { Avatar } from '@mui/material';
-import { projects } from '../../static/data/projects.js'
+import { Avatar, Typography } from '@mui/material';
+import { projects } from '../../static/data/projects.js';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    image: {
+        '&:hover': {
+            cursor: 'pointer',
+            'box-shadow': 'rgba(0, 0, 0, 0.22) 0px 30px 45px',
+            transform: 'translate3d(0px, -4px, 0px)',
+        }
+    },
+}));
+
+
 
 const ProjectIndex = () => {
+
+    const classes = useStyles();
 
     return (
         <div>
             <Grid container spacing={2} direction='row' style={{ display: 'flex', maxWidth: 1100, margin: '0 auto', alignItems: 'center', justifyContent: 'center' }}>
                 {projects.map((project) => (
-                <Grid item key={project.id} sm={6} >
-                  <Avatar variant='square' sx={{height: '100%', width: '100%', border: '0.1px solid lightgray' }} src={require(`../../static/images/${project.title}.png`).default}/>
+                <Grid item key={project.id} sm={6}>
+                 <Typography variant='subtitle2' align='center'>{project.title}</Typography>
+                  <Avatar variant='square' sx={{height: '100%', width: '100%', border: '0.1px solid lightgray' }} src={require(`../../static/images/${project.title}.png`).default} className={classes.image} />
                 </Grid>
                 ))}
             </Grid>
