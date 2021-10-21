d9351ef2842790fadfc41d04484287d6cca1c073
DJ Taylor <dtaylor88@gmail.com>
2021-10-20 17:19:40 -0700

    add scroll to on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import { Grid, Typography, Divider, IconButton } from '@material-ui/core'
+import { Grid, Typography, Divider, IconButton, Button } from '@material-ui/core'
 import { Avatar } from '@mui/material';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
@@ -8,8 +8,8 @@ import Image from '../static/images/IMG_6865.png';
 import Skills from '../Skills';
 import WorkIcon from '@material-ui/icons/Work';
 import Tooltip from '@mui/material/Tooltip';
-
-
+import { Link, Element, animateScroll as scroll } from 'react-scroll'
+import { LinearScale } from '@material-ui/icons';
 
 const About = (props) => {
     const { about, skills } = props
@@ -38,16 +38,24 @@ const About = (props) => {
             <Avatar src={Image} variant='square' style={{ margin: 'auto', height: '50%', width: '50%', marginTop: '1rem' }} />
             </div>
           </Carousel>
+          <Grid item style={{ margin: '1rem' }}>
+            <Link activeClass="active" to="skills" spy={true} smooth={true}  duration={500}>
+                <Button style={{ display: 'flex', margin: '0 auto', color: mode === 'dark' ? '#fff' : '#000' }} variant='text'>View Skills and Link to Resume</Button>
+             </Link>
+          </Grid>
         </Grid>
         <Grid item sm={6} style={{ marginBottom: '2rem' }}>
-            <Typography variant='h4' align='center'>Notable Skills</Typography>
+        <Element name='skills'>
+            <Typography variant='h5' align='center'>Notable Skills & Resume</Typography>
             <Divider style={{ background: mode === 'dark' ? 'rgba(255,255,255,0.12)' : 'rgba(0,0,0,0.12)' }}/>
             <Skills skills={skills} />
-            <Tooltip title='View Resume' arrow>
-            <IconButton onClick={handleResume} style={{ display: 'flex', margin: '0 auto', marginTop: '4rem', color: mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.54)' }} >
-             <WorkIcon fontSize='large' />
+            <Tooltip title='View Resume' arrow placement='top'>
+            <IconButton onClick={handleResume} style={{ display: 'flex', margin: '0 auto', marginTop: '2rem', marginBottom: '1rem', color: mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.54)' }} >
+             <WorkIcon style={{ transform: 'scale(2.5)' }} />
             </IconButton>
             </Tooltip>
+            <Button style={{ display: 'flex', margin: '0 auto', color: mode === 'dark' ? '#fff' : '#000'}} onClick={() => scroll.scrollToTop()} variant='text'>Back to Top</Button>
+        </Element>
         </Grid>
         </Grid>
         </div>
