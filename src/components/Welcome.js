28a4570545581d0ecb5908d602b5779d290c4f6d
DJ Taylor <dtaylor88@gmail.com>
2021-10-23 12:46:06 -0700

    change spacing on landing page, edit about me

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -16,12 +16,12 @@ const Welcome = () => {
 
     return (
         <div>
-            <Grid container spacing={1} direction='row' style={{display: 'flex', maxWidth: 1200, margin: '0 auto', justifyContent: 'center', alignItems: 'center', marginTop: '5rem', marginBottom: '10rem' }}>
-            <Grid item sm={6}>
-            <Typography variant='h1'>DJ Taylor</Typography>
+            <Grid container spacing={1} direction='row' style={{display: 'flex', margin: '0 auto', maxWidth: 800, justifyContent: 'center', alignItems: 'center', marginTop: '5rem', marginBottom: '5rem' }}>
+            <Grid item xs={6}>
+            <Typography variant='h2'>DJ Taylor</Typography>
             <Typography variant='subtitle1'>Full Stack Developer | Musician</Typography>
             </Grid>
-            <Grid item sm={6}>
+            <Grid item xs={6} style={{ display: 'flex', margin: '0 auto', alignItems:'center', justifyContent: 'center' }}>
             <Avatar src={Image} className={classes.sizeAvatar} />    
             </Grid>
             </Grid>
