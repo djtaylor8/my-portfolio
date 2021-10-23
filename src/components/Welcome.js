9f011153354c981bf21848e24897a56947c0dc8e
DJ Taylor <dtaylor88@gmail.com>
2021-10-23 13:10:27 -0700

    change margins on home page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,6 +8,8 @@ const useStyles = makeStyles((theme) => ({
     sizeAvatar: {
         height: theme.spacing(50),
         width: theme.spacing(50),
+        display: 'flex',
+        margin: '0 auto'
     },
 }));
 
@@ -16,12 +18,14 @@ const Welcome = () => {
 
     return (
         <div>
-            <Grid container spacing={1} direction='row' style={{display: 'flex', margin: '0 auto', maxWidth: 800, justifyContent: 'center', alignItems: 'center', marginTop: '5rem', marginBottom: '5rem' }}>
-            <Grid item xs={6}>
-            <Typography variant='h2'>DJ Taylor</Typography>
+            <Grid container spacing={1} direction='row' style={{display: 'flex', margin: '0 auto', maxWidth: 1000, justifyContent: 'center', alignItems: 'center', marginTop: '3rem', marginBottom: '5rem' }}>
+            <Grid item xs={6} style={{ display: 'flex', margin: '0 auto', flexDirection: 'column' }}>
+            <div style={{ margin: '0 auto' }}>
+            <Typography variant='h1'>DJ Taylor</Typography>
             <Typography variant='subtitle1'>Full Stack Developer | Musician</Typography>
+            </div>
             </Grid>
-            <Grid item xs={6} style={{ display: 'flex', margin: '0 auto', alignItems:'center', justifyContent: 'center' }}>
+            <Grid item xs={6}>
             <Avatar src={Image} className={classes.sizeAvatar} />    
             </Grid>
             </Grid>
