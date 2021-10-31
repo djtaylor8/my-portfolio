9dc8803cf29c8ed3b9a0a58ca59676dac2389ce6
DJ Taylor <dtaylor88@gmail.com>
2021-10-31 11:32:23 -0700

    updated resume

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -26,7 +26,7 @@ const About = (props) => {
 
 
     const handleResume =() => {
-        window.open('https://drive.google.com/file/d/1Ezf6Ic7cBaHiaUfryU_E1ht4xYJ6Grqd/view?usp=sharing')
+        window.open('https://drive.google.com/file/d/1u3Gck8ghdc8aBhCsWS-T_zBcdhgQpsRN/view?usp=sharing')
     }
 
     return (
