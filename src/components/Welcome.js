1fb1d73c9dfba12f55fbc3667c6ccf9ad1c3a148
DJ <dtaylor88@gmail.com>
2021-12-07 18:15:20 -0800

    update about media query

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Grid, Typography } from '@material-ui/core';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Image from '../static/images/IMG_9375.png';
 import { Avatar } from '@mui/material';
 import { makeStyles } from '@material-ui/core/styles';
 
+const theme = createTheme({
+    breakpoints: {
+      values: {
+       xs: 0,
+       sm: 450,
+       md: 600,
+       lg: 900,
+       xl: 1200,
+       tablet:1024
+     }
+   }
+   }); 
+
 const useStyles = makeStyles((theme) => ({
     sizeAvatar: {
-        height: theme.spacing(50),
-        width: theme.spacing(50)
+        height: theme.spacing(45),
+        width: theme.spacing(45)
     },
 }));
 
@@ -16,22 +30,21 @@ const Welcome = () => {
     const classes = useStyles();
     const largeScreen = useMediaQuery(theme => theme.breakpoints.up('md'));
     const mobile = useMediaQuery(theme => theme.breakpoints.down('xs'));
-    debugger;
 
     return (
-        <div>
+        <ThemeProvider theme={theme}>
             <Grid container spacing={2} direction={largeScreen ? 'row' : 'column'} style={{display: 'flex', maxWidth: mobile ? '1224px' : 1200, margin: '0 auto', justifyContent: 'center', alignItems: 'center', marginTop: '3rem', marginBottom: '5rem' }}>
             <Grid item xs={largeScreen ? 6 : 12} style={{ display: 'flex', alignItems:'center', justifyContent: 'center', margin: '0 auto' }}>
             <div>
-            <Typography variant='h1'>DJ Taylor</Typography>
+            <Typography variant={mobile ? 'h2' : 'h1'} align='center'>DJ Taylor</Typography>
             <Typography variant='subtitle1' align='center'>Full Stack Developer | Musician</Typography>
             </div>
             </Grid>
-            <Grid item xs={largeScreen ? 6 : 12} style={{ marginTop: '2rem' }}>
+            <Grid item xs={largeScreen ? 6 : 12} style={{ marginTop: '2rem', display: 'flex', alignItems: 'center', justifyContent: 'center', margin:'0 auto' }}>
             <Avatar src={Image} className={classes.sizeAvatar} />    
             </Grid>
             </Grid>
-        </div>
+        </ThemeProvider>
     );
 };
 
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Grid, Typography, Divider } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Avatar } from '@mui/material';
 import Image from '../../static/images/IMG_6865.png';
+
+const theme = createTheme({
+    breakpoints: {
+      values: {
+       xs: 0,
+       sm: 450,
+       md: 600,
+       lg: 900,
+       xl: 1200,
+       tablet:1024
+     }
+   }
+   }); 
+
 
 const useStyles = makeStyles((theme) => ({
   sizeAvatar: {
@@ -14,25 +29,28 @@ const useStyles = makeStyles((theme) => ({
 
 const About = (props) => {
     const largeScreen = useMediaQuery(theme => theme.breakpoints.up('md'));
+    const mobile = useMediaQuery(theme => theme.breakpoints.down('xs'));
     const classes = useStyles();
     const { about } = props
     const mode = localStorage.getItem('darkMode')
 
     return (
+        <ThemeProvider theme={theme}>
         <div>
-         <Grid container spacing={6} direction={largeScreen ? 'row' : 'column'} style={{maxWidth: 1100, display: 'flex', margin: '0 auto', marginTop: '2px', marginLeft: '2px', marginBottom: '2rem', alignItems: 'center', justifyContent: 'space-around' }}>
-            <Grid item xs={8} style={{ marginBottom: '1rem'}} >
+         <Grid container spacing={6} direction={largeScreen ? 'row' : 'column'} style={{maxWidth: mobile ? '1224px' : 1200, display: 'flex', margin: '0 auto', marginTop: '2px', marginLeft: '2px', marginBottom: '2rem', alignItems: 'center', justifyContent: 'space-around' }}>
+            <Grid item xs={mobile ? 10: 8} style={{ marginBottom: '1rem'}} >
             <Typography variant='h4' align='center'>About Me</Typography>
             <Divider style={{ marginBottom: '1rem', background: mode === 'dark' ? 'rgba(255, 255, 255, 0.12)' : 'rgba(0,0,0,0.12)' }}/>
             <Typography style={{ marginBottom: '1rem' }}>{about.bio}</Typography>
             <Typography style={{ marginBottom: '1rem' }}>{about.dev}</Typography>
             <Typography>{about.end}</Typography>
             </Grid>
-            <Grid item xs={largeScreen ? 2 : 8} >
+            <Grid item xs={largeScreen ? 2 : 12} >
             <Avatar src={Image} variant='circle' className={classes.sizeAvatar} />
             </Grid>
         </Grid>
         </div>
+        </ThemeProvider>
     );
 };
 
